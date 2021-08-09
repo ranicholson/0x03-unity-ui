107db8370e85dd22b33e2e1dfc4ae84b5fe423f5
Allen Nicholson <66395341+ranicholson@users.noreply.github.com>
2021-08-09 18:03:12 -0500

    Updated for assignment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private int score = 0;
 
     public float speed = 5f;
+    public Image winloseBG;
     public int health = 5;
     public Rigidbody rb;
+    public Text healthText;
+    public Text scoreText;
+    public Text winloseText;
 
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
     void FixedUpdate()
     {
@@ -27,32 +38,53 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(0, 0, -speed * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (health == 0)
-        {
-            Debug.Log("Game Over!");
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Pickup")
         {
             score++;
-            Debug.Log ("Score: " + score);
+            SetScoreText();
             Object.Destroy(other.gameObject);
         }
 
         if (other.tag == "Trap")
         {
             health--;
-            Debug.Log("Health: " + health);
+            SetHealthText();
         }
 
         if (other.tag == "Goal")
-            Debug.Log("You win!");
+        {
+            winloseText.text = "You Win!";
+            winloseText.color = Color.black;
+            winloseBG.color = Color.green;
+            winloseBG.gameObject.SetActive(true);
+        }
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            winloseText.text = "Game Over!";
+            winloseText.color = Color.white;
+            winloseBG.color = Color.red;
+            winloseBG.gameObject.SetActive(true);
+            StartCoroutine(LoadScene(3));
+        }
+
+        if (Input.GetKey("escape"))
+            SceneManager.LoadScene("menu");
     }
 }
